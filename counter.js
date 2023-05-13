a37b43ac7c82fb7e84e23a408908a0ef0d4f03fc
Monteiro3456 <123958151+Monteiro3456@users.noreply.github.com>
2023-05-13 16:22:29 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,377 @@
+// items 
+const menu =  [
+  
+  { id:1,
+    title:"buttermilk pancakes",
+    category:"breakfast",
+    price:15.99,
+    img:"https://th.bing.com/th/id/OIP.2vnCVHlZLaytK1OiWhJ32gHaJQ?pid=ImgDet&rs=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  { id:2,
+    title:"dinner double",
+    category:"lunch",
+    price:13.99,
+    img:"https://media.cntraveler.com/photos/586eb8449d2b725a71af8123/16:9/w_1280,c_limit/hot-dogs-gene-and-judes-photo-credit-Nick-Holmes.jpg",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  { id:23,
+    title:"American breakfast",
+    category:"breakfast",
+    price:23.99,
+    img:"https://th.bing.com/th/id/OIP.2suce3n7Fo0NoRMvkCLd8wHaE6?pid=ImgDet&rs=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  
+  { id:24,
+    title:"Fried Eggs",
+    category:"breakfast",
+    price:13.99,
+    img:"https://th.bing.com/th/id/OIP.jOylmnAMGoWput_MCgLz_gHaHa?pid=ImgDet&w=500&h=500&rs=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+
+  { id:25,
+    title:"Rice curry",
+    category:"lunch",
+    price:13.99,
+    img:"https://i0.wp.com/kinaraindiancuisine.ca/wp-content/uploads/2020/05/Lunch-Thali_Kinaraindiancuisine-scaled.jpg?fit=2560%2C2048&ssl=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  
+  { id:26,
+    title:"Fried Rice",
+    category:"lunch",
+    price:23.99,
+    img:"https://th.bing.com/th/id/OIP.rHkLrjKwQoGNH50uvgmhegHaE6?pid=ImgDet&rs=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+
+
+];
+
+const breakfastMenu = [
+
+  { id:3,
+    title:"American breakfast",
+    category:"breakfast",
+    price:23.99,
+    img:"https://th.bing.com/th/id/OIP.2suce3n7Fo0NoRMvkCLd8wHaE6?pid=ImgDet&rs=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  
+  { id:4,
+    title:"Fried Eggs",
+    category:"breakfast",
+    price:13.99,
+    img:"https://th.bing.com/th/id/OIP.jOylmnAMGoWput_MCgLz_gHaHa?pid=ImgDet&w=500&h=500&rs=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+
+  { id:8,
+    title:"Masala Dosa",
+    category:"breakfast",
+    price:13.99,
+    img:"https://www.mistay.in/travel-blog/content/images/2020/05/cover-1-5.jpg",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  { id:38,
+    title:"Chicken pattice",
+    category:"breakfast",
+    price:13.99,
+    img:"https://www.fittoservegroup.com/wp-content/uploads/2016/08/keto-chicken-patties-on-a-white-plate.jpg",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+
+
+
+
+
+
+]
+
+const lunchMenu = [
+
+  { id:5,
+    title:"Rice curry",
+    category:"lunch",
+    price:13.99,
+    img:"https://i0.wp.com/kinaraindiancuisine.ca/wp-content/uploads/2020/05/Lunch-Thali_Kinaraindiancuisine-scaled.jpg?fit=2560%2C2048&ssl=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  
+  { id:6,
+    title:"Fried Rice",
+    category:"lunch",
+    price:23.99,
+    img:"https://th.bing.com/th/id/OIP.rHkLrjKwQoGNH50uvgmhegHaE6?pid=ImgDet&rs=1",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+
+  { id:7,
+    title:"Chicken lolipop",
+    category:"lunch",
+    price:13.99,
+    img:"https://www.ruchikrandhap.com/wp-content/uploads/2017/09/Chicken-Lollipops-Indo-Chinese-3-1.jpg",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  { id:37,
+    title:"Chicken Biryani",
+    category:"lunch",
+    price:33.99,
+    img:"https://th.bing.com/th/id/R.14fb8fdd0364511290a86322f5591e65?rik=4ehlm633kiGHNQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2ff%2ffe%2fChicken_Biryani.jpg&ehk=4tKFw5pgho4h615hHZ9Mhb%2fojSD2LpdSMGxtbCdgmIE%3d&risl=&pid=ImgRaw&r=0",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  { id:72,
+    title:"crockets",
+    category:"lunch",
+    price:43.99,
+    img:"https://media.gettyimages.com/photos/traditional-spanish-croquetas-picture-id897521536",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  
+
+  
+
+
+
+
+
+
+]
+
+
+const shakesMenu = [
+
+ 
+  { id:9,
+    title:"Mango Shake",
+    category:"shake",
+    price:13.99,
+    img:"https://img.food.boxspace.in/image/rbk_5af07e2ccb89e/xhdpi.jpg",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  { id:10,
+    title:"Banana Shake",
+    category:"shake",
+    price:13.99,
+    img:"https://lh3.googleusercontent.com/proxy/_6sg3xpSkVltZPqtziJaDDL7uDNXV0uw3gYMHSRcH6dHA0FlbSI-5UkbzztbdD2pb32tFdNiNzMrcRx2oftxWPWpXJmAbPls64mbmFmaoNFCqQ=w1200-h630-p-k-no-nu",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  { id:11,
+    title:"Strawbery Shake",
+    category:"shake",
+    price:13.99,
+    img:"https://th.bing.com/th/id/R.3f26f5e011a542ecfc454771c5d5ca6d?rik=%2f4GTjtc9mYneOw&riu=http%3a%2f%2feclecticrecipes.com%2fwp-content%2fuploads%2f2012%2f02%2fstrawberry-milkshake-1-600x900.jpg&ehk=wOm%2bjEtQdv%2fue4nmMfF4qAdKKYe5zEP7Akun6kQIqD4%3d&risl=&pid=ImgRaw&r=0",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+  { id:12,
+    title:"Chocolate Shake",
+    category:"shake",
+    price:13.99,
+    img:"https://i.pinimg.com/originals/ba/a0/d7/baa0d7dc1e9512cc74bc8075c0cfea58.jpg",
+    desc:"I'm baby woke milkshake wolf bitters live edge blue bottle, hammock freegan mug whatever cold pressed"
+    
+  },
+
+
+
+
+
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+const sectionCenter =  document.querySelector('.section-center');
+const breakfastBtn = document.querySelector('.breakfast');
+const lunchBtn = document.querySelector('.lunch');
+const shakesBtn = document.querySelector('.shakes');
+const allBtn = document.querySelector('.all');
+
+
+
+
+
+// display all items
+function displayAllItems () {
+
+let displayMenu = menu.map(function(item){
+    
+    return `   <article class="menu-item">
+    <img src=${item.img} class="photo"
+      alt=${item.title}>
+
+
+ 
+  <div class="item-info">
+    <header>
+      <h4>${item.title} </h4>
+      <h4 class="price">${item.price}</h4>
+      <div class="underline-p"></div>
+    </header>
+
+   <p class="item-text">${item.desc}
+   </p> 
+  </div>
+  </article>`
+  });
+
+
+displayMenu = displayMenu.join('');
+// console.log(displayMenu);
+
+sectionCenter.innerHTML = displayMenu;
+
+
+}
+
+
+
+
+// display breakfast menu
+
+breakfastBtn.addEventListener('click', function () {
+
+  let displayMenu = breakfastMenu.map(function(item){
+    
+    return `   <article class="menu-item">
+    <img src=${item.img} class="photo"
+      alt=${item.title}>
+
+
+ 
+  <div class="item-info">
+    <header>
+      <h4>${item.title} </h4>
+      <h4 class="price">${item.price}</h4>
+      <div class="underline-p"></div>
+    </header>
+
+   <p class="item-text">${item.desc}
+   </p> 
+  </div>
+  </article>`
+  });
+
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+
+});
+
+
+displayAllItems();
+// display all items
+allBtn.addEventListener('click',displayAllItems);
+
+// display lunch items 
+
+lunchBtn.addEventListener('click',function () {
+
+  let displayMenu = lunchMenu.map(function(item){
+    
+    return `   <article class="menu-item">
+    <img src=${item.img} class="photo"
+      alt=${item.title}>
+
+
+ 
+  <div class="item-info">
+    <header>
+      <h4>${item.title} </h4>
+      <h4 class="price">${item.price}</h4>
+      <div class="underline-p"></div>
+    </header>
+
+   <p class="item-text">${item.desc}
+   </p> 
+  </div>
+  </article>`
+  });
+
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+
+
+});
+
+// display shakes menu
+shakesBtn.addEventListener('click',function () {
+
+  let displayMenu = shakesMenu.map(function(item){
+    
+    return `   <article class="menu-item">
+    <img src=${item.img} class="photo"
+      alt=${item.title}>
+
+
+ 
+  <div class="item-info">
+    <header>
+      <h4>${item.title} </h4>
+      <h4 class="price">${item.price}</h4>
+      <div class="underline-p"></div>
+    </header>
+
+   <p class="item-text">${item.desc}
+   </p> 
+  </div>
+  </article>`
+  });
+
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
